c0717567dbadcd451ec3d0c6d7e39ac0984fd9d1
michaelgarv <michaelgarv@cox.net>
2023-06-08 19:22:27 -0700

    json data

--- Michael_Folder/deliverable/app.py
+++ Michael_Folder/deliverable/app.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import sqlite3
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
@@ -11,7 +11,7 @@ from flask import Flask, jsonify, render_template
 #################################################
 # Database Setup
 #################################################
-engine = create_engine("sqlite:///data/deathStats.sqlite")
+engine = create_engine("sqlite:///data/deathSTATSKEY.sqlite")
 
 # reflect an existing database into a new model
 Base = automap_base()
@@ -38,20 +38,42 @@ def main_page():
 
 @app.route("/api/v1.0/cods")
 def cods():
-  
+    # Connect to the SQLite database
     session = Session(engine)
 
-    # Query all passengers
-    results = session.query(COD.Year).all()
+    # Query all rows from the table
+    results = session.query(COD).all()
 
-    session.close()
-
+    # Define an empty list to store the JSON objects
     cods_list = []
-    for Year, State in results:
-        cods_dict = {}
-        cods_dict["Year"] = Year
+
+    # Iterate over the results and convert each row to a dictionary
+    for row in results:
+        cods_dict = {
+            "id": row.id,
+            "year": row.year,
+            "state": row.state,
+            "all_causes": row.all_causes,
+            "natural_cause": row.natural_cause,
+            "septicemia": row.septicemia,
+            "malignant_neoplasms": row.malignant_neoplasms,
+            "diabetes": row.diabetes,
+            "alzheimers": row.alzheimers,
+            "influenza_pneumonia": row.influenza_pneumonia,
+            "lower_respiratory": row.lower_respiratory,
+            "other_respiratory": row.other_respiratory,
+            "nephritis": row.nephritis,
+            "abnormal": row.abnormal,
+            "heart_disease": row.heart_disease,
+            "cerebrovascular_diseases": row.cerebrovascular_diseases,
+            "covid": row.covid
+        }
         cods_list.append(cods_dict)
 
+    # Close the session
+    session.close()
+
+    # Return the JSON response
     return jsonify(cods_list)
 
 if __name__ == '__main__':
